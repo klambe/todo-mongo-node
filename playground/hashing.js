e660e0b0c92370cf002de7c454e133bacae1a0b2
Kevin <klambe17@gmail.com>
2017-08-17 14:41:04 +0100

    added jwt hashing to playground

---
+++ playground/hashing.js
@@ -0,0 +1,57 @@
+const {SHA256} = require('crypto-js');
+
+const jwt = require('jsonwebtoken');
+
+
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, 'secretSaltValue');
+
+console.log(token);
+
+var decoded = jwt.verify(token, 'secretSaltValue');
+
+console.log(decoded);
+
+
+// var message = 'I am user number three';
+//
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// var data = {
+//     id: 4
+// };
+//
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) +'somesecret').toString()
+// }
+//
+//
+//
+//
+// var resultHash = SHA256(JSON.stringify(token.data)+'somesecret').toString();
+//
+// if(resultHash === token.hash){
+//     console.log('data was not changed');
+// }else {
+//     console.log('dont trust data changed');
+// }
+//
+// token.data.id = 5;
+// token.hash  = SHA256(JSON.stringify(token.data)).toString();
+//
+// console.log('****Data was changed***');
+//
+//
+// if(resultHash === token.hash){
+//     console.log('data was not changed');
+// }else {
+//     console.log('dont trust data changed');
+// }
