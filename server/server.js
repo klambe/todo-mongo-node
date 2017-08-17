6a479efa5319a7404333004f6c408bc41cd7586f
Kevin <klambe17@gmail.com>
2017-08-17 13:30:07 +0100

    added user post route to project

--- server/server.js
+++ server/server.js
@@ -107,4 +107,16 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User( body );
+
+    user.save().then((doc) => {
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 module.exports = {app};
