ec3c78bb32fba89d032c7529ce461d0ce2e0aa57
Kevin <klambe17@gmail.com>
2017-08-19 19:31:38 +0100

    removed config variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,27 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env *******', env);
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI ='mongodb://localhost:27017/TodoApp';
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+
+    var envConfig = config[env];
+
+    // process.env.PORT = envConfig['PORT'];
+    // process.env.MONGODB_URI = envConfig['MONGODB_URI'];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
-else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI ='mongodb://localhost:27017/TodoAppTest';
-}
+
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI ='mongodb://localhost:27017/TodoApp';
+// }
+// else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI ='mongodb://localhost:27017/TodoAppTest';
+// }
 
 console.log('database uri *******', process.env.MONGODB_URI);
 
