6ca6105459879f12fa551402766fc8307ab00a1c
Kevin <klambe17@gmail.com>
2018-01-15 16:19:16 +0000

    changed config.json

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 console.log('env *******', env);
 
 if (env === 'development' || env === 'test') {
-    var config = require('./config.json');
+    var config = require('config.json');
 
     var envConfig = config[env];
 
